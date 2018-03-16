5d03281a2629344fccd42134e325ddea50632920
timzhangliang <798534509@qq.com>
2018-03-16 14:23:40 +0800

    2

--- TestCode/TestCode/Form1.cs
+++ TestCode/TestCode/Form1.cs
@@ -21,8 +21,6 @@ namespace TestCode
         {
             textBox1.Text = "1";
             textBox1.Text = "2";
-            textBox1.Text = "3";
-            textBox1.Text = "4";
         }
     }
 }
